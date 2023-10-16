8256ccd2f32454714aeb9d132695a6c1df4a8b98
jchaz22 <jason.chaskin@gmail.com>
2023-10-16 12:53:57 -0400

    fixed openning link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -58,7 +58,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="category/learn-about-dl"
+            to="/introduction"
           >
             Get started
           </Link>
